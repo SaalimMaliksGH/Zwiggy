3a672f8ff20d58b372404c0458b5a0db2588bbc0
SaalimMaliksGH <maliksaalim14@gmail.com>
2024-07-05 17:15:35 +0530

    first-commit

---
+++ back-end/server.js
@@ -0,0 +1,37 @@
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js";
+import { foodRouter } from "./routes/foodRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import cartRouter from "./routes/cartRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+
+
+//app config
+const app = express();
+const port = process.env.port || 4000;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+
+//DB connection
+connectDB();
+
+//api endpoint
+app.use('/api/food', foodRouter);
+app.use('/images', express.static('uploads'));
+app.use('/api/user', userRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
+
+
+app.get('/', (req, res) => {
+    res.send("api working");
+})
+
+
+app.listen(port, () => {
+    console.log(`Server Started on http://localhost:${port}`)
+})
